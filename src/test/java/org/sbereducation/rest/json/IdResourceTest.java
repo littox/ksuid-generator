61e39bdd0462496a417019aea5091fcf0815a231
Dmitry Zbirko <dzbirko@sbereducation.ru>
2022-02-10 13:01:40 +0300

    [SU-138] add integrations tests/dependencies

--- src/test/java/org/sbereducation/rest/json/IdResourceTest.java
+++ src/test/java/org/sbereducation/rest/json/IdResourceTest.java
@@ -1,21 +1,33 @@
 package org.sbereducation.rest.json;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-
 @QuarkusTest
-public class IdResourceTest {
+class IdResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    @DisplayName("Успешно получить KSUID")
+    void successfullyGetKSUID() {
         given()
-          .when().get("/id")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+            .when().get("/id")
+            .then()
+            .statusCode(200)
+            .body("id", notNullValue());
     }
+
+    @Test
+    @DisplayName("Должен вернуть 404, если URL не существует")
+    void shouldReturnNotFoundWhenUrlDoesNotExist() {
+        given()
+            .when().get("/fail")
+            .then()
+            .statusCode(404);
+    }
+
 
 }
--- src/test/java/org/sbereducation/rest/json/NativeIdResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.sbereducation.rest.json;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeIdResourceIT extends IdResourceTest {
-
-    // Execute the same tests but in native mode.
-}
