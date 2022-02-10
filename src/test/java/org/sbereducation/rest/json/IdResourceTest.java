8f3425f7f8c8d3ded410458deaa85384c5dd40b7
Dmitry Zbirko <dzbirko@sbereducation.ru>
2022-02-10 19:26:40 +0300

    [SU-138] add integrations tests/ sorted KSUID

--- src/test/java/org/sbereducation/rest/json/IdResourceTest.java
+++ src/test/java/org/sbereducation/rest/json/IdResourceTest.java
@@ -4,6 +4,12 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
 import io.quarkus.test.junit.QuarkusTest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +35,22 @@ class IdResourceTest {
             .statusCode(404);
     }
 
-
+    @Disabled
+    @Test
+    @DisplayName("Сгенерированные KSUIDы сортируются")
+    synchronized void generatedIdsAreSorted() {
+        TreeMap<Integer, String> testMap = new TreeMap<>();
+        List<String> listString = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            String responseId =
+                given()
+                    .when().get("/id").then().extract().path("id");
+            testMap.put(i, responseId);
+            listString.add(responseId);
+        }
+        Collections.sort(listString);
+        for (Entry<Integer, String> integerStringEntry : testMap.entrySet()) {
+            assert integerStringEntry.getValue().equals(listString.get(integerStringEntry.getKey()));
+        }
+    }
 }
